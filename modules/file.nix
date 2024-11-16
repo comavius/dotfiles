0042284d5900c4df5e6d1fa27c698544d9da0c06
comavius <comavius@proton.me>
2024-11-16 16:23:34 +0900

    🚧

--- modules/file.nix
+++ modules/file.nix
@@ -1,13 +1,11 @@
-{username, pkgs, ...}: {
+{username, ...}: {
   home = {
     homeDirectory = "/home/${username}";
     file = {
-      ".profile".text = ''
+      ".home-manager/.profile".text = ''
         if type zsh > /dev/null 2>&1; then
           export SHELL="zsh"
           exec zsh
-        else
-          exec ${pkgs.bash}/bin/bash
         fi
       '';
     };
