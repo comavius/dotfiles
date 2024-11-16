91d69959b45e6c180d1e6d405d90c8a8aac3d6af
comavius <comavius@proton.me>
2024-11-16 16:49:52 +0900

    🚧

--- modules/file/default.nix
+++ modules/file/default.nix
@@ -1,10 +1,19 @@
-{username, ...}: {
+{pkgs, username, ...}: let
+  home-manager-directory = pkgs.stdenv.mkDerivation {
+    name = "home-manager-directory";
+    src = ./.;
+    phases = "installPhase";
+    installPhase = ''
+      mkdir -p $out
+      cp -r * $out
+    '';
+  };
+in
+ {
   home = {
     homeDirectory = "/home/${username}";
     file = {
-      ".home-manager/.bashrc"  = {
-        source = ./.bashrc;
-      };
+      ".home-manager".source = home-manager-directory;
     };
   };
 }
