af7b1f1bab141ada7b889dc51b3e25c24084f7ca
comavius <comavius@proton.me>
2024-11-16 16:29:31 +0900

    🚧

--- modules/file.nix
+++ modules/file.nix
@@ -2,7 +2,7 @@
   home = {
     homeDirectory = "/home/${username}";
     file = {
-      ".home-manager/.zshrc".text = ''
+      ".home-manager".".bashrc".text = ''
         if type zsh > /dev/null 2>&1; then
           export SHELL="zsh"
           exec zsh
