bd2ab8c242a043818a226e79695e2d36d45406b7
comavius <comavius@proton.me>
2024-11-16 19:00:27 +0900

    ✨ sway.Desktop

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -2,4 +2,13 @@
   wayland.windowManager.sway = {
     enable = true;
   };
+  home.file = {
+    ".home-manager/sway/sway.Desktop".text = ''
+      [Desktop Entry]
+      Name=Sway
+      Comment=An i3-compatible Wayland compositor
+      Exec=sway
+      Type=Application
+    '';
+  };
 }
