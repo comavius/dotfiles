2041edd54e4b6781ea18da2622941f0f9faf339b
comavius <comavius@proton.me>
2024-11-16 19:33:06 +0900

    ✨ sway extra packages

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -1,12 +1,18 @@
 { ... }: {
   wayland.windowManager.sway = {
     enable = true;
+    extraPackages = pkgs: with pkgs; [
+      swaybg
+      swayidle
+      swaylock
+      waybar
+    ];
   };
   home.file = {
     ".home-manager/sway/sway.desktop".text = ''
       [Desktop Entry]
       Name=Sway
-      Comment=An i3-compatible Wayland compositor
+      Comment=An cd-compatible Wayland compositor
       Exec=sway
       Type=Application
     '';
