a5c133a9f134bcf093ce8cbed8a38867350f7a24
comavius <comavius@proton.me>
2024-11-18 02:45:04 +0900

    🚧

--- modules/i18n/default.nix
+++ modules/i18n/default.nix
@@ -11,8 +11,8 @@
     };
   };
   programs.zsh.localVariables = {
-    GTK_IM_MODULE = "fcitx5";
-    QT_IM_MODULE = "fcitx5";
-    XMODIFIERS = "@im=fcitx5";
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
   };
 }
