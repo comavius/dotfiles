f84ec0f00aa951f2bb09dfcc0cc3b619e7eeb40a
comavius <comavius@proton.me>
2024-11-18 01:26:41 +0900

    🚧

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -3,5 +3,6 @@ args @ {...}: let
   waybar = import ./waybar.nix args;
   mako = import ./mako.nix args;
   launcher = import ./sway-launcher.nix args;
-in
-  sway // waybar // mako // launcher
+in {
+  inherit sway waybar mako launcher;
+}
