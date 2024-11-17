31060e9f7f987b3c4318eccfe595dbddfe84a73f
comavius <comavius@proton.me>
2024-11-18 01:35:29 +0900

    🚧

--- modules/sway/sway-launcher.nix
+++ modules/sway/sway-launcher.nix
@@ -2,4 +2,14 @@
   home.packages = [
     pkgs.sway-launcher-desktop
   ];
+  home.file.".home-manager/sway/sway-launcher.sh" = {
+    text = ''
+      #!${pkgs.zsh}/bin/zsh
+      ${pkgs.sway-launcher-desktop}/bin/sway-launcher-desktop &
+      sleep 0.1
+      swaymsg '[title="sway-launcher-desktop"]' floating enable
+      swaymsg '[title="sway-launcher-desktop"]' focus
+    '';
+    executable = true;
+  };
 }
