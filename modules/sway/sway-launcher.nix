d57e17c68537034a4ffe3dd04506567faf64e7c9
comavius <comavius@proton.me>
2024-11-18 01:36:49 +0900

    🚧

--- modules/sway/sway-launcher.nix
+++ modules/sway/sway-launcher.nix
@@ -5,7 +5,7 @@
   home.file.".home-manager/sway/sway-launcher.sh" = {
     text = ''
       #!${pkgs.zsh}/bin/zsh
-      ${pkgs.sway-launcher-desktop}/bin/sway-launcher-desktop &
+      kitty ${pkgs.sway-launcher-desktop}/bin/sway-launcher-desktop &
       sleep 0.1
       swaymsg '[title="sway-launcher-desktop"]' floating enable
       swaymsg '[title="sway-launcher-desktop"]' focus
