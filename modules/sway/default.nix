d00f8399023083df99619507f753ca71ca72ab36
comavius <comavius@proton.me>
2024-11-17 20:41:01 +0900

    🚧

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -1,4 +1,8 @@
-{username, pkgs, ...}: {
+{
+  username,
+  pkgs,
+  ...
+}: {
   wayland.windowManager.sway = {
     enable = true;
     config = {
@@ -6,7 +10,9 @@
       terminal = "kitty";
       startup = [
       ];
-    bars = [ ];
+      bars = [
+        #"waybar"
+      ];
     };
     wrapperFeatures.gtk = true;
   };
@@ -21,13 +27,45 @@
           "eDP-1"
           "HDMI-A-1"
         ];
-        modules-left = ["sway/workspaces" "sway/mode" "wlr/taskbar"];
+        modules-left = ["sway/workspaces" "wlr/taskbar"];
         modules-center = ["sway/window"];
-        modules-right = ["mpd" "temperature"];
+        modules-right = [
+          "pulseaudio"
+          "memory"
+          "cpu"
+          "battery"
+          "clock"
+        ];
 
         "sway/workspaces" = {
           disable-scroll = true;
           all-outputs = true;
+          format = "{icon} {name}";
+        };
+        "clock" = {
+          "format" = "{:%Y/%m/%d %H:%M}";
+        };
+        "cpu" = {
+          "interval" = 10;
+          "format" = "CPU: {usage}%";
+        };
+        "memory" = {
+          "interval" = 10;
+          "format" = "RAM: {used:0.1f}GiB/{total:0.1f}GiB ({percentage}%) SWAP: {swapUsed:0.1f}GiB/{swapTotal:0.1f}GiB ({swapPercentage}%)";
+        };
+        "battery" = {
+          "bat" = "BAT0";
+          "states" = {
+            "good" = 95;
+            "warning" = 30;
+            "critical" = 10;
+          };
+          "format" = "BAT0: {capacity}%";
+          "format-charging" = "BAT0: {capacity}% (charging)";
+          "format-plugged" = "BAT0: {capacity}% (plugged)";
+        };
+        "pulseaudio" = {
+          "format" = "{volume}%";
         };
       };
     };
@@ -43,7 +81,7 @@
       DesktopNames=sway
     '';
     ".home-manager/sway/entry.sh" = {
-        text = ''
+      text = ''
         #!${pkgs.zsh}/bin/zsh
         source /home/${username}/.pshenv
         source /home/${username}/.zshrc
