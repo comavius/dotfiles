888b46101e9dfa8630e5363238d503b7cfb33874
comavius <comavius@proton.me>
2024-11-17 15:15:11 +0900

    🚧

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -20,6 +20,7 @@
     '';
     ".home-manager/sway/sway-session.sh".text = ''
       #!/home/${username}/.nix-profile/bin/zsh
+      echo $PATH
       su - ${username} -c "source /home/${username}/.zshrc"
       echo $PATH
       nixGLMesa sway 2&> /home/${username}/.home-manager/sway/sway.log
