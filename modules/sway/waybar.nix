5f2525ed2c2cc397da4b5c883b52255317d86470
comavius <comavius@proton.me>
2024-11-30 20:04:32 +0900

    🐛 waybar for all displays

--- modules/sway/waybar.nix
+++ modules/sway/waybar.nix
@@ -8,8 +8,7 @@
         position = "top";
         height = 30;
         output = [
-          "eDP-1"
-          "HDMI-A-1"
+          "*"
         ];
         modules-left = [
           "sway/workspaces"
