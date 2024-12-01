d709135538c841fcb2bc28f1df7b06a8bfc69793
comavius <comavius@proton.me>
2024-12-01 16:01:14 +0900

    🚧 ✨manual keybindings

--- modules/sway/sway.nix
+++ modules/sway/sway.nix
@@ -15,13 +15,16 @@ let
       magick convert $src/hardy_wallpaper_uhd.png -modulate 80,40,220 $out/wallpaper.png
     '';
   };
+  workspace = number: "workspace number ${toString number}";
+  terminal = "kitty";
+  menu = "rofi -show drun";
 in
 {
   wayland.windowManager.sway = {
     enable = true;
     config = {
       modifier = "Mod4";
-      terminal = "kitty";
+      inherit terminal menu;
 
       startup =
         [
@@ -38,7 +41,6 @@ in
           command = "${pkgs.waybar}/bin/waybar";
         }
       ];
-      menu = "rofi -show drun";
       floating.criteria = [
         { title = "sway-launcher-desktop"; }
       ];
@@ -51,6 +53,43 @@ in
           tap = "enabled";
         };
       };
+      modes = {
+        resize = {
+          Down = "exec swaymsg resize grow height 10 px";
+          Escape = "exec swaymsg mode default";
+          Left = "exec swaymsg resize shrink width 10 px";
+          Return = "exec swaymsg mode default";
+          Right = "exec swaymsg resize grow width 10 px";
+          Up = "exec swaymsg resize shrink height 10 px";
+        };
+
+      };
+      keybindings =
+        let
+          win = "Mod4";
+          hyper = "shift";
+        in
+        {
+          "${win}+1" = "${workspace 1}";
+          "${win}+2" = "${workspace 2}";
+          "${win}+3" = "${workspace 3}";
+          "${win}+4" = "${workspace 4}";
+          "${win}+5" = "${workspace 5}";
+          "${win}+6" = "${workspace 6}";
+          "${win}+7" = "${workspace 7}";
+          "${win}+8" = "${workspace 8}";
+          "${win}+9" = "${workspace 9}";
+          "${win}+0" = "${workspace 10}";
+          "${win}+r" = "mode resize";
+          "${win}+${hyper}+q" = "kill";
+          "${win}+Return" = "exec ${terminal}";
+          "${win}+d" = "exec ${menu}";
+
+          "${win}+Left" = "move left";
+          "${win}+Down" = "move down";
+          "${win}+Up" = "move up";
+          "${win}+Right" = "move right";
+        };
     };
     extraConfig = ''
       output * bg ${wallpaper}/wallpaper.png stretch
